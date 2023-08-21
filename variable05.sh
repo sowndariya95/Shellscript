229e0e23c637989abe23d6dab9694406d6debc08
banu <aa@aa.in>
2023-08-21 21:09:26 +0530

    variable05

---
+++ variable05.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter your username"
+
+read -s Username
+
+echo "Username entered is $Username"
+
+echo "Please enter your password"
+
+read -s Password
+
+echo "Password entered is $Password"
