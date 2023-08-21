3e5c10e1e5ed294d96c5c8fd69208c3302a35645
banu <aa@aa.in>
2023-08-21 22:14:25 +0530

    function-09

---
+++ function09.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+Root=$(id -u)
+
+validate(){
+    if[ $1 -ne 0 ]
+    then
+    echo "Installation is failure"
+    else
+    echo "Installation is success"
+    fi
+}
+
+if [ $Root -ne 0 ]
+then
+echo "Execute the command with sudo permission"
+exit 1
+fi
+
+yum install mysql -y
+
+validate $?
+
+ yum install postfix -y
+
+validate $?
