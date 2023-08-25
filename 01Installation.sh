334b6f3194a464f6557115c5d9b86ee83c2c0a74
banu <aa@aa.in>
2023-08-25 19:31:06 +0530

    installation

--- 01Installation.sh
+++ 01Installation.sh
@@ -7,31 +7,40 @@ DATE=$(date +%F)
 Script_Name=$0
 Logfile=/tmp/$Script_Name-$DATE.log
 Package=("git" "mysql" "postfix" "mailx")
+User_permission=$(id -u)
+
+if [ $User_permission -ne 0 ]
+then 
+    echo -e "$R Error : $N Run with root user privelage"
+    exit 1
+fi
 
 validate(){
-    if [ $1 -ne 0 ]
-    then 
-      echo -e "$R Error : $N Run with root user privelage"
-      exit 1
-    fi
+
+if [ $1 -ne 0 ]
+then
+    echo -e "$G Installation is $i success $N"
+else 
+     echo -e "$R Package is already exist $N"
+fi
 }
 
 Installation(){
-    for i in $Package
-    do
-      if [ sudo yum list --installed | grep $i -ne 0 ]
-      then
-        sudo yum install -y $i &>>$Logfile
-        echo -e "$G Installation is $i success $N"
-      else
-        echo -e "$R Package is already exist $N"
-     fi
-    done
+for i in ${Package[@]}
+do
+  if [ sudo yum list --installed | grep $i -ne 0 ]
+  then
+    sudo yum install -y $i &>>$Logfile
+    validate $? &>>$Logfile
+  fi
+done
 }
 
-User_permission=$(id -u)
+Installation $? &>>$Logfile
 
-validate $? &>>$Logfile
+
+
+
 
 
 
